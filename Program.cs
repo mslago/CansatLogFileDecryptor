34402776edb84a7fec6970627c593458850851c7
Marcelo Lago <72633651+mslago@users.noreply.github.com>
2025-06-01 15:58:02 +0100

    feat: deal with existing output files.

--- Program.cs
+++ Program.cs
@@ -8,13 +8,31 @@ namespace CanSatLogsDecryptor
         public static void Main(string[] args)
         {
             string filepath = args[0];
+            string outputfile;
+
             if (string.IsNullOrEmpty(filepath))
             {
                 Console.WriteLine("Please provide a file path as an argument.");
                 return;
             }
+
             Decrpytor decryptor = new();
-            decryptor.Decryptor(filepath, $"{filepath}-DECRYPTED.txt");
+
+            if (!File.Exists(filepath))
+            {
+                Console.WriteLine($"File not found: {filepath}");
+                return;
+            }
+            int i = 1;
+            while (File.Exists($"{filepath}-{i}-DECRYPTED.txt"))
+            {
+                i++;
+            }
+            outputfile = $"{filepath}-{i}-DECRYPTED.txt";
+
+            Console.WriteLine($"Output will be saved to: outputfile");
+
+            decryptor.Decryptor(filepath, outputfile);
         }
     }
 }
